78bb34730d90dffccd88b81f43636e9f388827a4
Camila Souza <74924931+Camila-cesar@users.noreply.github.com>
2022-07-07 16:10:59 -0300

    CopiaValores.java
    5 – Crie um arquivo chamado CopiaValores.java. Implemente um programa em Java que declare
    uma variável do tipo float chamada numero. Essa variável deve ser inicializada com o valor 3.14.
    Depois, declare outra variável do tipo float chamada numeroCopia. Essa variável deve ser
    inicializada com uma cópia do valor armazenado na variável numero. Por fim, exiba os valores
    armazenados nessas variáveis na saída padrão.

---
+++ Lista 2/CopiaValores.java
@@ -0,0 +1,20 @@
+/*
+ * CopiaValores.java
+ * Camila de Paula SOuza César
+ * Lista 02 de exercícios - Exercício 05 
+ * 
+ * 
+ */
+
+
+public class CopiaValores {
+	
+	public static void main (String[] args) {
+		
+		float numero = 3.14f;
+		float numeroCopia = numero;
+		
+		System.out.println ("numero = "+numero);
+		System.out.println ("numeroCopia = "+numeroCopia);
+	}
+}
