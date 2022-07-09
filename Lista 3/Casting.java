cb3745da5e53da57148003373df5bf9d554fd9dd
Camila Souza <74924931+Camila-cesar@users.noreply.github.com>
2022-07-09 16:33:08 -0300

    Casting.java
    2 - Crie um arquivo chamado Casting.java. Crie uma variável chamada numero1 do tipo long e
    inicialize-a com o valor 2147483648L. Depois, crie uma variável chamada numero2 do tipo int e
    copie o valor da variável numero1 para a variável numero2. Por fim, exiba, na saída padrão, os
    valores dessas duas variáveis.

---
+++ Lista 3/Casting.java
@@ -0,0 +1,23 @@
+/*
+ * Casting.java
+ * Camila de Paula Souza César 
+ * Lista 03 de exercícios - Exercício 2
+ * 
+ *
+ */
+
+
+public class Casting {
+	
+	public static void main (String[] args) {
+		
+		long numero1 = 2147483648L;
+		int numero2 = (int)numero1;
+		
+		
+		System.out.println(numero1);
+		System.out.println(numero2);
+		
+	}
+}
+
